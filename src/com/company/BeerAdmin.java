0fde167df51c018171824e85c823763d89af6898
DESKTOP-I2OVG7C\Drake <kaiden3991@gmail.com>
2017-01-20 09:20:55 +0100

    Created Class BeerAdmin with all methods throwing NotImplementedException

---
+++ src/com/company/BeerAdmin.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by Drake on 20.01.2017.
+ */
+public class BeerAdmin {
+    public BeerAdmin loadBeerStyles(){throw new NotImplementedException();}
+
+    public BeerAdmin printBeerStyles() {throw new NotImplementedException();}
+
+    public BeerAdmin printBeerStyles(String search) {throw new NotImplementedException();}
+
+    public BeerAdmin getBeerListForStyle(int idStyle) {throw new NotImplementedException();}
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here asfdd
+        System.out.println("Hello World, This. Is. The. BEERBASE");
     }
 }
